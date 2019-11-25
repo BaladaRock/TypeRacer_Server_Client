335b30c3482a8500afa78d8b1108559189bae0e7
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-11-25 22:45:58 +0200

    TypeRacer - added Server.cs and User.cs

---
+++ User/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+namespace User
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+            user.Init(Dns.GetHostEntry("localHost").AddressList[0].ToString(), 8000);
+            user.Send("Impostor");
+            Console.WriteLine(user.Receive());
+        }
+    }
+}
---
+++ User/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace User
+{
+    public class User
+    {
+        private bool initialized = false;
+        private static TcpClient client;
+        private static NetworkStream networkStream;
+        private static StreamReader streamReader;
+        private static StreamWriter streamWriter;
+
+        public static string Ip { get; set; }
+        public static int Port { get; set; }
+
+        public void Init(string ip, int port)
+        {
+            Ip = ip;
+            Port = port;
+
+            client = new TcpClient(new IPEndPoint(Dns.GetHostEntry("localHost").AddressList[0], Port));
+            networkStream = client.GetStream();
+            streamReader = new StreamReader(networkStream);
+            streamWriter = new StreamWriter(networkStream);
+
+            initialized = true;
+        }
+
+        public void Send(string data)
+        {
+            if (initialized)
+            {
+                streamWriter.WriteLine(data);
+                streamWriter.Flush();
+            }
+        }
+
+        public string Receive()
+        {
+            string msg = string.Empty;
+
+            if (initialized)
+            {
+                msg = streamReader.ReadLine();
+            }
+
+            return msg;
+        }
+    }
+}
